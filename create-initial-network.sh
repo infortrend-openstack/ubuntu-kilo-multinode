daf4f9c422e5c76b9a52291f146406ce331d1ddb
ryan chiang <c.ryan1002@gmail.com>
2015-05-16 02:32:55 +0800

    add gateway var

--- create-initial-network.sh
+++ create-initial-network.sh
@@ -22,6 +22,7 @@ if [[ "$FLOATING_IP_START" == "" ]]; then
     echo "Need to set floating IP start!"
     exit 0
 fi
+
 echo "Enter Floating IP End:  (ex:172.27.120.250): "
 read FLOATING_IP_END
 if [[ "$FLOATING_IP_END" == "" ]]; then
@@ -29,6 +30,12 @@ if [[ "$FLOATING_IP_END" == "" ]]; then
     exit 0
 fi
 
+echo "Enter Floating IP Gateway:  (ex:172.27.127.254): "
+read FLOATING_IP_GATEWAY
+if [[ "$FLOATING_IP_GATEWAY" == "" ]]; then
+    echo "Need to set floating IP gateway!"
+    exit 0
+fi
 
 echo "Start Create Virtual Network!"
 sleep 3
@@ -36,7 +43,7 @@ sleep 3
 source /root/admin-openrc.sh
  
 neutron net-create ext-net --router:external --provider:physical_network external --provider:network_type flat
-neutron subnet-create ext-net $FLOATING_IP_CIDR --name ext-subnet --allocation-pool start=$FLOATING_IP_START,end=$FLOATING_IP_END --disable-dhcp --gateway 172.27.127.254 --dns-nameserver 8.8.8.8
+neutron subnet-create ext-net $FLOATING_IP_CIDR --name ext-subnet --allocation-pool start=$FLOATING_IP_START,end=$FLOATING_IP_END --disable-dhcp --gateway $FLOATING_IP_GATEWAY --dns-nameserver 8.8.8.8
 neutron net-create private-net
 neutron subnet-create private-net 10.10.10.0/24 --name private-subnet --gateway 10.10.10.1 --dns-nameserver 8.8.8.8
 neutron router-create admin-router
