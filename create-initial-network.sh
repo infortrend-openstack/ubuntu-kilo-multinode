39f4e707827c918ef29eb1122beb2e5e563fefe5
johnnychou <johnny800503.work@gmail.com>
2016-11-02 15:19:36 +0800

    Update create-initial-network.sh
    Add configure DNS server

--- create-initial-network.sh
+++ create-initial-network.sh
@@ -8,6 +8,7 @@ fi
 FLOATING_IP_START=172.27.120.200
 FLOATING_IP_END=172.27.120.250
 FLOATING_IP_CIDR=172.27.112.0/20
+DNS_SERVER=192.168.99.16
 
 echo "Enter Floating IP CIDR:(ex:172.27.112.0/20): "
 read FLOATING_IP_CIDR
@@ -37,15 +38,22 @@ if [[ "$FLOATING_IP_GATEWAY" == "" ]]; then
     exit 0
 fi
 
+echo "Enter DNS:  (ex:192.168.99.16): "
+read DNS_SERVER
+if [[ "$DNS_SERVER" == "" ]]; then
+    echo "Need to set DNS server!"
+    exit 0
+fi
+
 echo "Start Create Virtual Network!"
 sleep 3
 
 source /root/admin-openrc.sh
  
 neutron net-create ext-net --router:external --provider:physical_network external --provider:network_type flat
-neutron subnet-create ext-net $FLOATING_IP_CIDR --name ext-subnet --allocation-pool start=$FLOATING_IP_START,end=$FLOATING_IP_END --disable-dhcp --gateway $FLOATING_IP_GATEWAY --dns-nameserver 8.8.8.8
+neutron subnet-create ext-net $FLOATING_IP_CIDR --name ext-subnet --allocation-pool start=$FLOATING_IP_START,end=$FLOATING_IP_END --disable-dhcp --gateway $FLOATING_IP_GATEWAY --dns-nameserver $DNS_SERVER
 neutron net-create private-net
-neutron subnet-create private-net 10.10.10.0/24 --name private-subnet --gateway 10.10.10.1 --dns-nameserver 8.8.8.8
+neutron subnet-create private-net 10.10.10.0/24 --name private-subnet --gateway 10.10.10.1 --dns-nameserver $DNS_SERVER
 neutron router-create admin-router
 neutron router-interface-add admin-router private-subnet
 neutron router-gateway-set admin-router ext-net
